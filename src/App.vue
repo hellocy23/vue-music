3fb2b9780e5a39feedf23215fb26e596c6d26636
cyh5kf <1269415602@qq.com>
2017-07-17 00:11:27 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
-    hello world
+    <m-header></m-header>
+    <tab></tab>
+    <router-view></router-view>
   </div>
 </template>
 
-<script>
+ <script type="text/ecmascript-6">
+  import MHeader from 'components/m-header/m-header'
+  import Tab from 'components/tab/tab'
 
+  export default {
+    components: {
+      MHeader,
+      Tab
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  @import "~common/stylus/variable"
-
-  #app
-    color: $color-theme
 
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Recommend from 'components/recommend/recommend'
+import Rank from 'components/rank/rank'
+import Search from 'components/search/search'
+import Singer from 'components/singer/singer'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/recommend',
+      component: Recommend
+    },
+    {
+      path: '/singer',
+      component: Singer
+    },
+    {
+      path: '/rank',
+      component: Rank
+    },
+    {
+      path: '/search',
+      component: Search
+    }
+  ]
+})
