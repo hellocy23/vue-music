b6010fa042d05b2fce6b95025d946ac9d9ef96c0
Chen Yu <chenyu@192.168.0.101>
2017-07-16 23:13:31 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  #app
+    color: $color-theme
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App'
+// import router from './router'
+
+import 'common/stylus/index.styl'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
