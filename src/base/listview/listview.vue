a9438b89739b5f93ed4fc06e52d2017e5388a1f3
cyh5kf <1269415602@qq.com>
2017-07-23 17:36:22 +0800

    fix singer

---
+++ src/base/listview/listview.vue
@@ -0,0 +1,118 @@
+<template>
+  <scroll class="listview" :data="data">
+    <ul>
+      <li v-for="(group, index) in data" class="list-group" :key="index">
+        <h2 class="list-group-title">{{group.title}}</h2>
+        <ul>
+          <li class="list-group-item" v-for="(item, index) in group.items" :key="index">
+            <img class="avatar" v-lazy="item.avatar" />
+            <span class="name">{{item.name}}</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+    <div class="list-shortcut">
+      <ul>
+        <li v-for="(item, index) in shortcutList" class="item" :key="index">
+          {{item}}
+        </li>
+      </ul>
+    </div>
+  </scroll>
+</template>
+
+<script type="text/ecmascript-6">
+  import Scroll from 'base/scroll/scroll'
+  // import Loading from 'base/loading/loading'
+  // import {getData} from 'common/js/dom'
+
+  // const TITLE_HEIGHT = 30
+  // const ANCHOR_HEIGHT = 18
+
+  export default {
+    props: {
+      data: {
+        type: Array,
+        default: []
+      }
+    },
+    computed: {
+      shortcutList() {
+        return this.data.map((group) => {
+          return group.title.substr(0, 1)
+        })
+      }
+    },
+    components: {
+      Scroll
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  .listview
+    width: 100%
+    height: 100%
+    overflow: auto
+    background: $color-background
+    .list-group
+      padding-bottom: 30px
+      .list-group-title
+        height: 30px
+        line-height: 30px
+        padding-left: 20px
+        font-size: $font-size-small
+        color: $color-text-l
+        background: $color-highlight-background
+      .list-group-item
+        display: flex
+        align-items: center
+        padding: 20px 0 0 30px
+        .avatar
+          width: 50px
+          height: 50px
+          border-radius: 50%
+        .name
+          margin-left: 20px
+          color: $color-text-l
+          font-size: $font-size-medium
+    .list-shortcut
+      position: absolute
+      z-index: 30
+      right: 0
+      top: 50%
+      transform: translateY(-50%)
+      width: 20px
+      padding: 20px 0
+      border-radius: 10px
+      text-align: center
+      background: $color-background-d
+      font-family: Helvetica
+      .item
+        padding: 3px
+        line-height: 1
+        color: $color-text-l
+        font-size: $font-size-small
+        &.current
+          color: $color-theme
+    .list-fixed
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      .fixed-title
+        height: 30px
+        line-height: 30px
+        padding-left: 20px
+        font-size: $font-size-small
+        color: $color-text-l
+        background: $color-highlight-background
+    .loading-container
+      position: absolute
+      width: 100%
+      top: 50%
+      transform: translateY(-50%)
+</style>
+
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="singer">
-      歌手页面
+    <ListView :data="singers"></ListView>
   </div>
 </template>
 
  <script type="text/ecmascript-6">
   import { getSingerList } from 'api/singer'
   import { ERR_OK } from 'api/config'
+  import Singer from 'common/js/singer'
+  import ListView from 'base/listview/listview'
+
+  const HOT_NAME = '热门'
+  const HOT_SINGER_LEN = 10
 
   export default {
     data() {
@@ -21,11 +26,56 @@
       _getSingerList() {
         getSingerList().then((res) => {
           if (res.code === ERR_OK) {
-            console.log(res.data.list)
-            this.singers = res.data.list
+            this.singers = this._normalizeSinger(res.data.list)
+            console.log(this.singers)
           }
         })
+      },
+      _normalizeSinger(list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+        list.forEach((item, index) => {
+          if (index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fsinger_name
+            }))
+          }
+          const key = item.Findex
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+          map[key].items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        })
+        // 为了得到有序列表，我们需要处理map
+        let hot = []
+        let ret = []
+        for (let key in map) {
+          let val = map[key]
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val)
+          } else if (val.title === HOT_NAME) {
+            hot.push(val)
+          }
+        }
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+        return hot.concat(ret)
       }
+    },
+    components: {
+      ListView
     }
   }
   
