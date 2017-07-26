75362e499ecc241bf52b76adfa152831be4da1cd
cyh5kf <1269415602@qq.com>
2017-07-26 23:46:27 +0800

    fix singer-detail

--- src/api/singer.js
+++ src/api/singer.js
@@ -16,3 +16,20 @@ export function getSingerList() {
   })
   return jsonp(url, data, options)
 }
+
+export function getSingerDetail(singerId) {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_track_cp.fcg'
+  const data = Object.assign({}, commonParams, {
+    hostUin: 0,
+    needNewCode: 0,
+    order: 'listen',
+    platform: 'yqq',
+    begin: 0,
+    num: 30,
+    songstatus: 1,
+    singermid: singerId,
+    g_tk: 5381
+  })
+  return jsonp(url, data, options)
+}
+
