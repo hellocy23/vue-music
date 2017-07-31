e2d852a7643d7cae249b9cc7c331c341b034fdee
cyh5kf <1269415602@qq.com>
2017-07-31 21:37:32 +0800

    fix rank

--- src/api/config.js
+++ src/api/config.js
@@ -7,7 +7,8 @@ export const commonParams = {
 }
 
 export const options = {
-  param: 'jsonpCallback'
+  param: 'jsonpCallback',
+  prefix: 'jp'
 }
 
 export const ERR_OK = 0
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -14,7 +14,7 @@
         <div class="recommend-list">
           <h1 class="list-title">热门歌单推荐</h1>
           <ul>
-            <li class="item" v-for="(item, index) in discList" :key="index">
+            <li @click="selectItem(item)" class="item" v-for="(item, index) in discList" :key="index">
               <div class="icon">
                 <img v-lazy="item.imgurl" width="60" height="60" />
               </div>
@@ -30,6 +30,7 @@
         <loading></loading>
       </div>
     </scroll>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -40,6 +41,7 @@
   import Scroll from 'base/scroll/scroll'
   import Loading from 'base/loading/loading'
   import {playlistMixin} from 'common/js/mixin'
+  import {mapMutations} from 'vuex'
 
   export default {
     mixins: [playlistMixin],
@@ -58,6 +60,12 @@
         const bottom = playList.length > 0 ? '60px' : ''
         this.$refs.recommend.style.bottom = bottom
         this.$refs.scroll.refresh()
+      },
+      selectItem(item) {
+        this.$router.push({
+          path: `/recommend/${item.dissid}`
+        })
+        this.setDisc(item)
       },
       _getRecommend() {
         getRecommend().then((res) => {
@@ -78,7 +86,10 @@
           this.$refs.scroll.refresh()
           this.checkLoaded = true
         }
-      }
+      },
+      ...mapMutations({
+        setDisc: 'SET_DISC'
+      })
     },
     components: {
       Slider,
