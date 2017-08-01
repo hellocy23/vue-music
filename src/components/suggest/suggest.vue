66ead714b42d325f436aada3ef2cab6c95a213de
cyh5kf <1269415602@qq.com>
2017-08-01 23:54:20 +0800

    fix search

--- src/components/suggest/suggest.vue
+++ src/components/suggest/suggest.vue
@@ -6,7 +6,7 @@
           @scrollToEnd="searchMore"
   >
     <ul class="suggest-list">
-      <li class="suggest-item" v-for="(item, index) in result" :key="index">
+      <li @click="selectItem(item)" class="suggest-item" v-for="(item, index) in result" :key="index">
         <div class="icon">
           <i :class="getIconCls(item)"></i>
         </div>
@@ -26,8 +26,8 @@
   import {search} from 'api/search'
   import {ERR_OK} from 'api/config'
   import {createSong} from 'common/js/song'
-//   import {mapMutations, mapActions} from 'vuex'
-//   import Singer from 'common/js/singer'
+  import {mapMutations, mapActions} from 'vuex'
+  import Singer from 'common/js/singer'
 
   const TYPE_SINGER = 'singer'
   const perpage = 20
@@ -89,6 +89,20 @@
           return `${item.name}-${item.singer}`
         }
       },
+      selectItem(item) {
+        if (item.type === TYPE_SINGER) {
+          const singer = new Singer({
+            id: item.singermid,
+            name: item.singername
+          })
+          this.$router.push({
+            path: `/search/${singer.id}`
+          })
+          this.setSinger(singer)
+        } else {
+          this.insertSong(item)
+        }
+      },
       _checkMore(data) {
         const song = data.song
         if (!song.list.length || (song.curnum + song.curpage * perpage) >= song.totalnum) {
@@ -113,7 +127,13 @@
           }
         })
         return ret
-      }
+      },
+      ...mapMutations({
+        setSinger: 'SET_SINGER'
+      }),
+      ...mapActions([
+        'insertSong'
+      ])
     },
     watch: {
       query() {
--- src/store/actions.js
+++ src/store/actions.js
@@ -31,3 +31,49 @@ export const randomPlay = function({commit}, {list}) {
   commit(types.SET_FULL_SCREEN, true)
   commit(types.SET_PLAYING_STATE, true)
 }
+
+export const insertSong = function({commit, state}, song) {
+  // 不能在mutation之外操作state值，所以这里加slice，返回state副本，不会改变原来的state
+  let playList = state.playList.slice()
+  let sequenceList = state.sequenceList.slice()
+  let currentIndex = state.currentIndex
+  // 记录当前歌曲
+  let currentSong = playList[currentIndex]
+  // 查找当前列表是否有待插入的歌曲并返回其索引
+  let fpIndex = findIndex(playList, song)
+  // 因为是插入歌曲，所以索引+1
+  currentIndex++
+  // 插入这首歌到当前索引位置
+  playList.splice(currentIndex, 0, song)
+  // 如果已经包含了这首歌
+  if (fpIndex > -1) {
+    // 如果当前插入的序号大于列表中的序号
+    if (currentIndex > fpIndex) {
+      playList.splice(fpIndex, 1)
+      currentIndex--
+    } else {
+      playList.splice(fpIndex + 1, 1)
+    }
+  }
+
+  let currentSIndex = findIndex(sequenceList, currentSong) + 1
+
+  let fsIndex = findIndex(sequenceList, song)
+
+  sequenceList.splice(currentSIndex, 0, song)
+
+  if (fsIndex > -1) {
+    if (currentSIndex > fsIndex) {
+      sequenceList.splice(fsIndex, 1)
+    } else {
+      sequenceList.splice(fsIndex + 1, 1)
+    }
+  }
+
+  commit(types.SET_PLAYLIST, playList)
+  commit(types.SET_SEQUENCE_LIST, sequenceList)
+  commit(types.SET_CURRENT_INDEX, currentIndex)
+  commit(types.SET_FULL_SCREEN, true)
+  commit(types.SET_PLAYING_STATE, true)
+}
+
